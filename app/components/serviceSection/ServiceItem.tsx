7367d39adeb6866c7e353321b9e87041c5afa124
vm1990-Git <v.m.1990@hotmail.com>
2024-08-06 09:22:04 -0300

    service item update 3

--- app/components/serviceSection/ServiceItem.tsx
+++ app/components/serviceSection/ServiceItem.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState, useCallback } from "react";
 import { IconType } from "react-icons";
 
 interface ServiceItemProps {
@@ -16,55 +16,48 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
 }) => {
   const [selected, setSelected] = useState(false);
 
-  const selectoToFalse = useCallback(() => {
-    setSelected(false);
-  }, []);
-
-  const selectoToTrue = useCallback(() => {
-    setSelected(true);
-  }, []);
+  const handleMouseEnter = useCallback(() => setSelected(true), []);
+  const handleMouseLeave = useCallback(() => setSelected(false), []);
 
   return (
-    <>
-      <div
-        className="flex flex-col items-center justify-center text-center text-wrap rounded-2xl h-60 w-60 font-semibold text-sm"
-        onMouseEnter={selectoToTrue}
-        onMouseLeave={selectoToFalse}
-      >
-        {selected ? (
-          <>
-            <div className="flex flex-col justify-center items-center w-lvw px-6 max-w-96 animate-fade-up animate-once">
-              <div className="flex flex-col items-center justify-center pb-2 ">
-                <IconComponent size={50} />
-                <span className="flex text-2xl">{title}</span>
-              </div>
-              <div className="flex justify-center flex-wrap gap-1 text-start animate-fade-up animate-once">
-                {Array.isArray(description) ? (
-                  description.map((d, index) => (
-                    <div key={index}>
-                      <span>▸{d}</span>
-                    </div>
-                  ))
-                ) : (
-                  <span className="w-80 text-center duration-400 text-sm animate-fade-up animate-once">
-                    {description}
-                  </span>
-                )}
-              </div>
-            </div>
-          </>
-        ) : (
-          <div className="flex flex-col sm:flex-col justify-center items-center gap-4 w-60 h-60">
-            <div className="flex justify-center ">
-              <IconComponent size={iconSize} />
-            </div>
-            <span className="text-center text-2xl pb-2 translate-y-1  md:w-full">
-              {title}
-            </span>
+    <div
+      className="flex flex-col items-center justify-center text-center text-wrap rounded-2xl h-60 w-60 font-semibold text-sm"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onTouchStart={handleMouseEnter}
+      onTouchEnd={handleMouseLeave}
+    >
+      {selected ? (
+        <div className="flex flex-col justify-center items-center w-lvw px-6 max-w-96 animate-fade-up animate-once">
+          <div className="flex flex-col items-center justify-center pb-2">
+            <IconComponent size={50} />
+            <span className="flex text-2xl">{title}</span>
           </div>
-        )}
-      </div>
-    </>
+          <div className="flex justify-center flex-wrap gap-1 text-start animate-fade-up animate-once">
+            {Array.isArray(description) ? (
+              description.map((d, index) => (
+                <div key={index}>
+                  <span>▸ {d}</span>
+                </div>
+              ))
+            ) : (
+              <span className="w-80 text-center duration-400 text-sm animate-fade-up animate-once">
+                {description}
+              </span>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col sm:flex-col justify-center items-center gap-4 w-60 h-60">
+          <div className="flex justify-center">
+            <IconComponent size={iconSize} />
+          </div>
+          <span className="text-center text-2xl pb-2 translate-y-1 md:w-full">
+            {title}
+          </span>
+        </div>
+      )}
+    </div>
   );
 };
 
