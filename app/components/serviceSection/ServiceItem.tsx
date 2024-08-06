a5ee0ab50806663a804d610f7aed01b00b1184a5
vm1990-Git <v.m.1990@hotmail.com>
2024-08-06 09:47:01 -0300

    contactForm update 2

--- app/components/serviceSection/ServiceItem.tsx
+++ app/components/serviceSection/ServiceItem.tsx
@@ -21,16 +21,18 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
 
   return (
     <div
-      className="flex flex-col items-center justify-center text-center text-wrap rounded-2xl h-60 w-60 font-semibold text-sm"
+      className="flex flex-col items-center justify-center text-center text-wrap rounded-2xl h-60 w-60 font-semibold text-sm select-none"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onTouchStart={handleMouseEnter}
       onTouchEnd={handleMouseLeave}
+      role="button"
+      aria-pressed={selected}
     >
       {selected ? (
         <div className="flex flex-col justify-center items-center w-lvw px-6 max-w-96 animate-fade-up animate-once">
           <div className="flex flex-col items-center justify-center pb-2">
-            <IconComponent size={50} />
+            <IconComponent size={50} aria-hidden="true" />
             <span className="flex text-2xl">{title}</span>
           </div>
           <div className="flex justify-center flex-wrap gap-1 text-start animate-fade-up animate-once">
@@ -50,7 +52,7 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
       ) : (
         <div className="flex flex-col sm:flex-col justify-center items-center gap-4 w-60 h-60">
           <div className="flex justify-center">
-            <IconComponent size={iconSize} />
+            <IconComponent size={iconSize} aria-hidden="true" />
           </div>
           <span className="text-center text-2xl pb-2 translate-y-1 md:w-full">
             {title}
